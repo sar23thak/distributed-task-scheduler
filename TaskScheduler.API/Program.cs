7b87848bf0ee661d12e238164cc7e056cf3c0061
Sarthak Sharma <sarthak.sharma.che20@itbhu.ac.in>
2026-03-28 15:52:14 +0530

    feat: scaffold solution with API, Worker, Core projects and project references

---
+++ TaskScheduler.API/Program.cs
@@ -0,0 +1,9 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Services will be registered here
+
+var app = builder.Build();
+
+// Middleware and routes will be configured here
+
+app.Run();
---
+++ TaskScheduler.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace TaskScheduler.Core;
+
+public class Class1
+{
+
+}
